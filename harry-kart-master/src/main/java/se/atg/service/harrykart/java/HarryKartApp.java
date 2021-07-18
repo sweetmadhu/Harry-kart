225cacdab92453b97a8e8dd2bcbfe89ec34b87bb
madhu <madhusin@adobe.com>
2021-07-18 23:26:24 +0530

    removed target

--- harry-kart-master/src/main/java/se/atg/service/harrykart/java/HarryKartApp.java
+++ harry-kart-master/src/main/java/se/atg/service/harrykart/java/HarryKartApp.java
@@ -15,38 +15,7 @@ import se.atg.service.harrykart.java.rest.model.inputRequest.Participant;
 
 @SpringBootApplication
 public class HarryKartApp {
-    public static void main(String ... args) throws JAXBException {
-
+    public static void main(String ... args){
         SpringApplication.run(HarryKartApp.class, args);
-        List<Participant> participantList = new LinkedList<Participant>();
-        Participant participant = new Participant();
-        participant.setLane(1);
-        participant.setBaseSpeed(123);
-        participant.setName("test");
-        participantList.add(participant);
-        HarryKart kart = new HarryKart();
-        kart.setNumberOfLoops(1);
-        kart.setParticipant(participantList);
-        Loop loop = new Loop();
-        List<Loop> loopList = new LinkedList<>();
-        List<Lane> list = new LinkedList<>();
-        Lane lane = new Lane();
-        lane.setValue(1);
-        lane.setNumber(-1);
-
-        Lane lane2 = new Lane();
-        lane.setValue(2);
-        lane.setNumber(-2);
-
-        list.add(lane);
-        list.add(lane2);
-
-        loop.setLane(list);
-        loop.setNumber(1);
-        loopList.add(loop);
-        kart.setLoop(loopList);
-        JAXBContext context = JAXBContext.newInstance(HarryKart.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.marshal(kart,System.out);
     }
 }
